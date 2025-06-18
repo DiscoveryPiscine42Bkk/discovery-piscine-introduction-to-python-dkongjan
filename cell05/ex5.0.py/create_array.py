dfc0cbcd499508f520828615fb6b2ab396e20036
dkongjan <tungmaetaetae@gmail.com>
2025-06-18 07:26:15 +0000

    ีupdate

--- cell05/ex5.0.py/create_array.py
+++ cell05/ex5.0.py/create_array.py
@@ -1,6 +1,5 @@
-def main():
-    # Define an array of numbers
-    numbers = [1, 2, 3, 4, 5]
+# Define an array of numbers
+numbers = [3, 7, 12, 19, 21, 25]
 
-    # Display the array on the screen
-    print("The array of numbers is:", numbers)
+# Display the array on the screen
+print("Array of numbers:", numbers)
