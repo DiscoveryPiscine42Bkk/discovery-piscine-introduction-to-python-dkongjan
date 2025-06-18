ac1fe2b52615a285a9770396a1fbcfc4e54e2a5f
dkongjan <tungmaetaetae@gmail.com>
2025-06-18 07:34:37 +0000

    update

--- cell05/ex5.1/play_with_arrays.py
+++ cell05/ex5.1/play_with_arrays.py
@@ -1,10 +1,9 @@
-def main():
-    # Define the original array of numbers
-    original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+# Define the original array
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
 
-    # Create a new array by adding 2 to each element in the original array
-    new_array = [x + 2 for x in original_array]
+# Create a new array by adding 2 to each element
+new_array = [x + 2 for x in original_array]
 
-    # Display both arrays
-    print("Original array:", original_array)
-    print("New array     :", new_array)
+# Display both arrays
+print("Original array:", original_array)
+print("New array:     ", new_array)
