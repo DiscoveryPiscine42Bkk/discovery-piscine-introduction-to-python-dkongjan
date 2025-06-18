254dede7d5863318e60729f5c5717fb2bc933b94
dkongjan <tungmaetaetae@gmail.com>
2025-06-18 07:19:19 +0000

    update

--- cell05/ex5.3/play_with_arrays.py
+++ cell05/ex5.3/play_with_arrays.py
@@ -1,18 +1,8 @@
-def main():
-    # Define the original array of numbers
-    original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+seen = set()
 
-    # Use a set to keep track of seen values and avoid duplicates
-    seen = set()
-    new_array = []
-
-    for x in original_array:
-        if x > 5:
-            new_value = x + 2
-            if new_value not in seen:
-                new_array.append(new_value)
-                seen.add(new_value)
-
-    # Display both arrays
-    print("Original array:", original_array)
-    print("New array (values > 5 + 2, no duplicates):", new_array)
+print("2 8 9 48 22 -12:")
+for value in original_array:
+    if value not in seen:
+        print(value, end=' ')
+        seen.add(value)
